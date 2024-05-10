0fcd7d5266cdb8e5f48253120624f287ec5843d7
Chukwunalu Egwali <egwalichukwunalu@gmail.com>
2024-05-10 22:01:50 +0100

    take control buttons to the right

---
+++ app.js
@@ -0,0 +1,11 @@
+
+const sections = document.querySelectorAll('.section');
+const sectionBtns = document.querySelector('.controls');
+const sectionBtn = document.querySelectorAll('.control');
+const allSection = document.querySelectorAll('.main-content');
+
+function pageTransition() {
+    for(let i = 0; i < sectionBtns.length; i++ ) {
+        
+    }
+}
