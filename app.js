c907d735365b542b59a5f8ec835dd26bd9cddd05
Chukwunalu Egwali <egwalichukwunalu@gmail.com>
2024-05-12 22:18:54 +0100

    Activate control buttons navigation using js

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const sectionBtns = document.querySelector('.controls');
 const sectionBtn = document.querySelectorAll('.control');
 const allSection = document.querySelectorAll('.main-content');
 
-function pageTransition() {
-    for(let i = 0; i < sectionBtns.length; i++ ) {
-        
+function pageTransitions() {
+    for(let i = 0; i < sectionBtn.length; i++ ) {
+        sectionBtn[i].addEventListener('click', function () {
+            let currentBtn = document.querySelectorAll('.active-btn');
+            currentBtn[0].classList = currentBtn[0].className.replace('active-btn', '')
+            this.className += ' active-btn';
+        })
     }
 }
+
+pageTransitions();
